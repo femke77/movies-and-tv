add9ba4e8557bf50f92299691433ad4ad6fe994b
femke77 <megan.meyers.388@gmail.com>
2025-02-12 23:30:21 -0800

    search results displaying in itemCard for movie

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,18 +1,18 @@
 import { useSearchTitleQuery } from '../hooks/useSearchAndDiscover';
 import { useParams } from 'react-router-dom';
 import { IMovie } from '../interfaces/IMovie';
-
+import ItemCard from '../components/ItemCard';
 const Results = () => {
   const { query } = useParams<{ query: string }>();
   const { data } = useSearchTitleQuery(query ?? '', '1');
   //   console.log(data, isLoading, error);
 
   return (
-    <div className='mt-30'>
+    <div className='mt-20 mx-4 flex flex-wrap gap-4'>
       {data &&
         data?.map((movie: IMovie) => (
           <div key={movie.id}>
-            <h1>{movie.title}</h1>
+            <ItemCard item={movie} itemType='movie' />
           </div>
         ))}
     </div>
