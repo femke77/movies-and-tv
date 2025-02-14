7d5ec5730c17903da47f2a1638947153abcad34f
femke77 <megan.meyers.388@gmail.com>
2025-02-13 23:29:03 -0800

    results width tweaks

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -4,7 +4,7 @@ import { IMovie } from "../interfaces/IMovie";
 import ItemCard from "../components/ItemCard";
 import { useRef, useEffect } from "react";
 
-// TODO fix for small number of search results
+// TODO past 1990px wide need extra media queries
 const Results = () => {
   const { query } = useParams<{ query: string }>();
   const { data = [], isLoading } = useSearchTitleQuery(query ?? "", "1");
@@ -27,13 +27,13 @@ const Results = () => {
       <h2 className="text-3xl font-bold mt-4 mb-8">
         Search results for '{query?.trim() || lastLetterRef.current}'
       </h2>
-      <div className="mx-auto">
+      <div className="ml-2">
         <div className="flex flex-wrap flex-1 gap-4 items-start ">
           {results.length > 0 ? (
             results.map((movie: IMovie) => (
               <div
                 key={`movie-${movie.id}`}
-                className="w-[calc(50%-15px)] sm:w-[calc(33%-10px)] md:w-[calc(25%-22.5px)] lg:w-[calc(21%-25px)] xl:max-w-[calc(17%-24px)]"
+                className="w-[calc(50%-15px)] sm:w-[calc(33%-10px)] md:w-[calc(25%-18.5px)] lg:w-[calc(26%-25px)] xl:max-w-[calc(19%-1px)]"
               >
                 <ItemCard item={movie} itemType="movie" />
               </div>
