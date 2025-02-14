962b8e91d1356fc3e8cfa9d7876d33fc2dfe9d53
femke77 <megan.meyers.388@gmail.com>
2025-02-13 16:59:04 -0800

    better styling of cards so they grow and shrink

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -5,49 +5,51 @@ import UserRating from './UserRating';
 import { getStrokeColor } from '../utils/helpers';
 import { useEffect, useState } from 'react';
 
+// ItemCard.tsx
 const ItemCard = ({ item, itemType }: { item: IMovie; itemType: string }) => {
   const formattedDate = dayjs(item.release_date).format('MMM D, YYYY');
   const [isVisible, setIsVisible] = useState(false);
   const strokeColor = getStrokeColor(item.vote_average ?? 0);
+  
   useEffect(() => {
     setIsVisible(true);
     return () => {
       setIsVisible(false);
     };
   }, []);
+  
   return (
     <div
-    className={`relative flex flex-col items-center justify-between flex-grow min-w-[180px] max-w-[250px] h-[375px] bg-black rounded-xl shadow-lg overflow-hidden 
-      transition-opacity duration-500 ease-linear ${
-        isVisible ? 'opacity-100' : 'opacity-0'
-      } `}
+      className={`relative flex flex-col items-center justify-between w-full bg-black rounded-xl shadow-lg overflow-hidden 
+        transition-opacity duration-500 ease-linear ${
+          isVisible ? 'opacity-100' : 'opacity-0'
+        }`}
     >
-      <Link to={`/${itemType}/${item.id}`}>
-        <img
-          className='w-full min-w-full h-72 object-cover rounded-lg'
-          src={
-            item.poster_path
-              ? `https://image.tmdb.org/t/p/w342/${item.poster_path}`
-              : '/no_poster_available.svg'
-          }
-        />
-        <div className='flex flex-col flex-grow items-start justify-start w-full pt-4 bg-black'>
-          <div className='relative -top-13 left-3 w-full'>
-            <div className='flex items-end'>
-              {/* TODO vote average N/A when not avaliable */}
+      <Link to={`/${itemType}/${item.id}`} className="w-full">
+        <div className="aspect-[2/3] w-full">
+          <img
+            className="w-full h-full object-contain"
+            src={
+              item.poster_path
+                ? `https://image.tmdb.org/t/p/w342/${item.poster_path}`
+                : '/no_poster_available.svg'
+            }
+          />
+        </div>
+        <div className="flex flex-col flex-grow items-start justify-start w-full pt-4 bg-black">
+          <div className="relative -top-13 left-3 w-full">
+            <div className="flex items-end">
               <UserRating
                 rating={item.vote_average ?? 0}
                 color={strokeColor}
-                width='w-12'
-                height='h-12'
+                width="w-12"
+                height="h-12"
               />
-              {/* TODO pill or chip and show genres conditionally */}
-              {/* <p className='ml-12'>{movieGenres[0]}</p> */}
             </div>
-            <h2 className='whitespace-pre max-w-[192px] overflow-hidden text-sm/6 font-bold -ml-2 mt-1'>
+            <h2 className="whitespace-pre max-w-[192px] overflow-hidden text-sm/6 font-bold -ml-2 mt-1">
               {item.title}
             </h2>
-            <p className='text-xs font-light -ml-2 '>
+            <p className="text-xs font-light -ml-2">
               {formattedDate} &#x2022;{' '}
               {itemType.substring(0, 1).toUpperCase() + itemType.substring(1)}
             </p>
@@ -57,5 +59,6 @@ const ItemCard = ({ item, itemType }: { item: IMovie; itemType: string }) => {
     </div>
   );
 };
+
 
 export default ItemCard;
--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -5,27 +5,28 @@ import ItemCard from "../components/ItemCard";
 
 // TODO query movies and tv shows and merge them with a property of movie or tv & add the chips
 
+// Results.tsx
 const Results = () => {
   const { query } = useParams<{ query: string }>();
   const { data } = useSearchTitleQuery(query ?? "", "1");
-  //   console.log(data, isLoading, error);
 
   return (
     <div className="mt-20 mx-4">
-      <h2 className="text-3xl font-bold mt-4 mb-8 ">
+      <h2 className="text-3xl font-bold mt-4 mb-8">
         Search results for '{query}'
       </h2>
-  
-      <div className="flex flex-wrap flex-grow gap-4 justify-center">
+      
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(180px,1fr))] gap-4 items-start">
         {data?.map((movie: IMovie) => (
-          <div key={movie.id}>
+          <div 
+            key={movie.id} 
+            className="min-w-[180px] max-w-[350px] w-full"
+          >
             <ItemCard item={movie} itemType="movie" />
           </div>
         ))}
       </div>
-    
     </div>
-  );
-};
+  );}
 
-export default Results;
+  export default Results;
