60da34a6fe208f098ed03464abeb4aa62a86176b
femke77 <megan.meyers.388@gmail.com>
2025-02-13 23:01:57 -0800

    removed grid and set calc widths with flex for Results.tsx

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -6,38 +6,42 @@ import { useRef, useEffect } from "react";
 
 // TODO fix for small number of search results
 const Results = () => {
-  const { query,} = useParams<{ query: string }>();
-  const { data = [], isLoading} = useSearchTitleQuery(query ?? "", "1");
+  const { query } = useParams<{ query: string }>();
+  const { data = [], isLoading } = useSearchTitleQuery(query ?? "", "1");
   const lastResultsRef = useRef<IMovie[]>([]);
   const lastLetterRef = useRef<string | null>(null);
-  
-  useEffect(() => {
-      if (query?.length===1) {
-          lastResultsRef.current = data;
-          lastLetterRef.current = query[query.length - 1];
-        }
-    }, [query, data]);
-    
-    const results = query ? data : lastResultsRef.current;
 
-    if (isLoading) return null;
+  useEffect(() => {
+    if (query?.length === 1) {
+      lastResultsRef.current = data;
+      lastLetterRef.current = query[query.length - 1];
+    }
+  }, [query, data]);
+
+  const results = query ? data : lastResultsRef.current;
+
+  if (isLoading) return null;
 
   return (
     <div className="mt-20 mx-4">
       <h2 className="text-3xl font-bold mt-4 mb-8">
         Search results for '{query?.trim() || lastLetterRef.current}'
       </h2>
-
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(min(50%-10px,350px),1fr))] sm:grid-cols-[repeat(auto-fit,minmax(min(180px,350px),1fr))] gap-4 items-start">
-        {results.length > 0 ? (
-          results.map((movie: IMovie) => (
-            <div key={`movie-${movie.id}`} className="min-w-[100px] sm:min-w-[180px] max-w-[350px] w-full">
-              <ItemCard item={movie} itemType="movie" />
-            </div>
-          ))
-        ) : (
-          <p className="text-lg text-gray-400">No results found.</p>
-        )}
+      <div className="mx-auto">
+        <div className="flex flex-wrap flex-1 gap-4 items-start ">
+          {results.length > 0 ? (
+            results.map((movie: IMovie) => (
+              <div
+                key={`movie-${movie.id}`}
+                className="w-[calc(50%-15px)] sm:w-[calc(33%-10px)] md:w-[calc(25%-22.5px)] lg:w-[calc(21%-25px)] xl:max-w-[calc(17%-24px)]"
+              >
+                <ItemCard item={movie} itemType="movie" />
+              </div>
+            ))
+          ) : (
+            <p className="text-lg text-gray-400">No results found.</p>
+          )}
+        </div>
       </div>
     </div>
   );
