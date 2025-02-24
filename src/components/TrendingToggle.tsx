38a063348fd46a87b6b380b77367e938bf9960c6
femke77 <megan.meyers.388@gmail.com>
2025-02-23 16:44:43 -0800

    minor changes to home page

--- src/components/TrendingToggle.tsx
+++ src/components/TrendingToggle.tsx
@@ -1,11 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-const TrendingToggle = ({ onTimeChange }:{onTimeChange: (value:string)=>void}) => {
-  const [selected, setSelected] = useState('week');
-  const [translateX, setTranslateX] = useState(100); 
+const TrendingToggle = ({
+  onTimeChange,
+}: {
+  onTimeChange: (value: string) => void;
+}) => {
+  const [selected, setSelected] = useState("week");
+  const [translateX, setTranslateX] = useState(100);
 
   useEffect(() => {
-    setTranslateX(selected === 'day' ? 0 : 100);
+    setTranslateX(selected === "day" ? 0 : 100);
   }, [selected]);
 
   const handleChange = (value: string) => {
@@ -16,7 +20,7 @@ const TrendingToggle = ({ onTimeChange }:{onTimeChange: (value:string)=>void}) =
   return (
     <div className="relative h-[30px] rounded-[15px] bg-[#1c4b91]/20">
       <div className="relative flex h-full">
-        <div 
+        <div
           className={`
             absolute h-[30px] w-[100px] rounded-[15px]
             bg-gradient-to-r from-[#5c676b] to-[#3d3737]
@@ -28,13 +32,13 @@ const TrendingToggle = ({ onTimeChange }:{onTimeChange: (value:string)=>void}) =
         />
 
         <button
-          onClick={() => handleChange('day')}
+          onClick={() => handleChange("day")}
           className="relative z-10 flex h-full w-[100px] cursor-pointer items-center justify-center text-sm text-white transition-colors duration-300"
         >
           Today
         </button>
         <button
-          onClick={() => handleChange('week')}
+          onClick={() => handleChange("week")}
           className="relative z-10 flex h-full w-[100px] cursor-pointer items-center justify-center text-sm text-white transition-colors duration-300"
         >
           This Week
--- src/hooks/useTopRated.ts
+++ src/hooks/useTopRated.ts
@@ -19,7 +19,7 @@ export const useTopRatedMovies = () => {
           observer.disconnect();
         }
       },
-      { threshold: 0.1, rootMargin: '50px 0px' },
+      { threshold: 0.0, rootMargin: '50px 0px' },
     );
 
     const target = document.getElementById('top-section');
