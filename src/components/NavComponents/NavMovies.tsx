7b33cfb59301f9156365fd572d8facc5b201abea
femke77 <megan.meyers.388@gmail.com>
2025-02-25 19:13:01 -0800

    upgraded css for nav bar components

--- src/components/NavComponents/NavMovies.tsx
+++ src/components/NavComponents/NavMovies.tsx
@@ -1,6 +1,7 @@
-import { NavLink } from 'react-router-dom';
+
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/16/solid';
+import MenuItemLink from './MenuItemLink';
 
 const NavMovies = () => {
   return (
@@ -21,31 +22,42 @@ const NavMovies = () => {
       <MenuItems
         transition
         anchor='bottom end'
-        className='w-52 origin-top-right rounded-xl border border-white/5 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:12px] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0 relative z-50 bg-gray-900/95'
+        className='w-64 origin-top-right rounded-xl border border-white/5 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:12px] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0  bg-gray-900/95'
+      > <div className='flex justify-start items-center p-4 '>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='#f8f7f7'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+        className='lucide lucide-film'
       >
+        <rect width='18' height='18' x='3' y='3' rx='2' />
+        <path d='M7 3v18' />
+        <path d='M3 7.5h4' />
+        <path d='M3 12h18' />
+        <path d='M3 16.5h4' />
+        <path d='M17 3v18' />
+        <path d='M17 7.5h4' />
+        <path d='M17 16.5h4' />
+      </svg>
+      <h3 className='text-md font-bold ml-3'>Movies</h3>
+    </div>
         <MenuItem>
-          <NavLink
-            to='/explore/movies'
-            className='group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10'
-          >
-            Trending Movies
-          </NavLink>
+    <MenuItemLink to='/explore/movies' name='Trending Movies' />
         </MenuItem>
         <MenuItem>
-          <NavLink
-            to='/explore/toprated'
-            className='group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10'
-          >
-            Top Rated
-          </NavLink>
+        <MenuItemLink to='/explore/toprated' name='Top Rated Movies' />
+         
         </MenuItem>
         <MenuItem>
-          <NavLink
-            to='/explore/popular'
-            className='group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10'
-          >
-            Popular{' '}
-          </NavLink>
+        <MenuItemLink to='/explore/popular' name='Popular Movies' />
+         
+    
         </MenuItem>
       </MenuItems>
     </Menu>
--- src/components/NavComponents/NavTVShow.tsx
+++ src/components/NavComponents/NavTVShow.tsx
@@ -1,19 +1,18 @@
-// import { NavLink } from 'react-router-dom';
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
-import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/16/solid';
-import { NavLink } from 'react-router-dom';
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/16/solid";
+import MenuItemLink from "./MenuItemLink";
 
-const NavTVShow = () => {
+const NavTVShows = () => {
   return (
     <Menu>
-      <MenuButton className='hover:cursor-pointer inline-flex items-center gap-2 rounded-md bg-gray-800 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-700 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white'>
+      <MenuButton className="hover:cursor-pointer relative inline-flex items-center gap-2 rounded-md bg-gray-800 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-700 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white">
         {({ open }) => (
           <>
             TV Shows
             {open ? (
-              <ChevronUpIcon className='size-4 fill-white/60' />
+              <ChevronUpIcon className="size-4 fill-white/60" />
             ) : (
-              <ChevronDownIcon className='size-4 fill-white/60' />
+              <ChevronDownIcon className="size-4 fill-white/60" />
             )}
           </>
         )}
@@ -21,37 +20,40 @@ const NavTVShow = () => {
 
       <MenuItems
         transition
-        anchor='bottom end'
-        className='w-52 origin-top-right rounded-xl border border-white/5 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:12px] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0 relative z-50 bg-gray-900/95
-        '
+        anchor="bottom end"
+        className="w-64 origin-top-right rounded-xl border border-white/5 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:12px] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0  bg-gray-900/95"
       >
-        <MenuItem>
-          <NavLink
-            to='/explore/tv'
-            className='group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10'
+        {" "}
+        <div className="flex justify-start items-center p-4 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#ffffff"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-tv"
           >
-            Trending TV
-          </NavLink>
+            <rect width="20" height="15" x="2" y="7" rx="2" ry="2" />
+            <polyline points="17 2 12 7 7 2" />
+          </svg>
+          <h3 className="text-md font-bold ml-3">TV Shows</h3>
+        </div>
+        <MenuItem>
+          <MenuItemLink to="/explore/tv" name="Trending Series" />
         </MenuItem>
         <MenuItem>
-          <NavLink
-            to='/explore/top-series'
-            className='group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10'
-          >
-            Top Series
-          </NavLink>
+          <MenuItemLink to="/explore/top-rated" name="Top Rated Series" />
         </MenuItem>
         <MenuItem>
-          <NavLink
-            to='/explore/popular-tv'
-            className='group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10'
-          >
-            Popular Shows
-          </NavLink>
+          <MenuItemLink to="/explore/popular-tv" name="Popular TV" />
         </MenuItem>
       </MenuItems>
     </Menu>
   );
 };
 
-export default NavTVShow;
+export default NavTVShows;
