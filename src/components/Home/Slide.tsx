08682cc9650cd72e360137daed59089aa1af23fa
femke77 <megan.meyers.388@gmail.com>
2025-02-25 20:16:19 -0800

    updated Slide css to break earlier

--- src/components/Home/Slide.tsx
+++ src/components/Home/Slide.tsx
@@ -5,6 +5,7 @@ import type { IItem } from '../../interfaces/IItem';
 import { useItemLogos } from '../../hooks/useAllTrendingWithLogoFetch';
 import genresData from '../../utils/data/genres.json';
 import { Link } from 'react-router-dom';
+import { useWindowSize } from '../../hooks/useWindowSize';
 
 // TODO refactor this css layout
 // TODO Background trasition and image box shadow
@@ -23,7 +24,7 @@ const Slide = ({
 }) => {
   const formattedMovieDate = dayjs(slide.release_date).format('MMM D, YYYY');
   const { genres } = genresData;
-
+  const { width } = useWindowSize();
   const logoFromQuery = useItemLogos(
     slide.id,
     slide.media_type ?? 'movie',
@@ -39,7 +40,8 @@ const Slide = ({
   });
 
   return (
-    <div className=' swiper-slide bg-black h-full flex items-center py-10'>
+    <div className={` swiper-slide bg-black h-full flex items-center py-10`}>
+
       {/* background image */}
       <div
         className={`relative w-full h-full bg-cover bg-center md:bg-top transition-opacity 
@@ -48,16 +50,21 @@ const Slide = ({
           backgroundImage: `url('https://image.tmdb.org/t/p/w1280${slide.backdrop_path}')`,
         }}
       >
-        {/* gradient overlay */}
-        <div className='absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-black to-transparent' />
+        {/* gradient overlays */}
+        <div className='absolute bottom-0 left-0 w-full h-1/8 sm:h-1/2 bg-gradient-to-t from-black to-transparent' />
         <div className='absolute inset-0 bg-gradient-to-r from-black via-black/30 sm:via-black/50 md:via-black/50 lg:via-black/50 to-transparent' />
 
         {/* card content */}
-        <div className='max-w-[1800px] mx-auto'>
+        <div 
+        className='max-w-[1800px] mx-auto'>
           {/* left, top - genre, release date, title logo */}
-          <div className='absolute md:w-1/2 h-full flex flex-col justify-center px-16 md:px-18 lg:px-26 xl:ml-10'>
-            <div className='flex flex-col items-center md:items-start'>
-              <div className='flex justify-start items-start mb-12 '>
+    <div className={`absolute flex flex-col px-16 md:px-18 lg:px-26 xl:ml-10 
+              ${width < 950 
+                ? 'w-full h-full justify-center' 
+                : 'w-1/2 top-1/2 transform -translate-y-1/2'}`}
+          >
+            <div className={`flex flex-col ${width < 950 ? 'items-center ': 'items-start'}`}>
+              <div className={`flex justify-start items-start mb-12 `}>
                 {movieGenres.length >= 1 &&
                   movieGenres.slice(0, 2).map((genre) => (
                     <span
@@ -83,7 +90,7 @@ const Slide = ({
 
             {/* left, mid - title or title logo, overview */}
             <Link to={`/${slide.media_type}/${slide.id}`}>
-              <div className='flex flex-col items-center md:items-start'>
+              <div className={`flex flex-col ${width <950 ? 'items-center': 'items-start' }`}>
                 {displayLogo ? (
                   <img
                     className='mb-8 w-64 h-auto'
@@ -101,8 +108,8 @@ const Slide = ({
             </Link>
 
             {/* left, bottom - rating and watch componentns */}
-            <div className='flex flex-row items-center justify-around mt-4 '>
-              <div className='mb-2'>
+            <div className={`flex flex-row items-center ${width<950 ?'justify-center':'justify-start'} mt-4 `}>
+              <div className='mb-2 mr-10'>
                 <WatchButton />
               </div>
               <div className='pl-4 mb-2'>
@@ -111,7 +118,7 @@ const Slide = ({
             </div>
           </div>
           {/* right, only - poster image */}
-          <div className='absolute right-0 top-1/2 transform -translate-y-1/2 mr-16 md:mr-24 lg:mr-48 hidden md:block'>
+          <div className={`absolute right-0 top-1/2 transform -translate-y-1/2 mr-16 md:mr-24 lg:mr-48   ${width < 950 ? 'hidden': 'block'}`}>
             {slide.poster_path && (
               <img
                 className='w-78 h-auto rounded-lg pt-10 '
