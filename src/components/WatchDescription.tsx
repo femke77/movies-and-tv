b43bf324827b901c5abdaa17ddfdaf436b29cded
femke77 <megan.meyers.388@gmail.com>
2025-03-03 01:13:53 -0800

    movie && fix

--- src/components/WatchDescription.tsx
+++ src/components/WatchDescription.tsx
@@ -22,9 +22,9 @@ const WatchDescription = ({
             viewBox="0 0 24 24"
             fill="none"
             stroke="#ffffff"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             className="lucide lucide-calendar-fold"
           >
             <path d="M8 2v4" />
--- src/pages/WatchMovie.tsx
+++ src/pages/WatchMovie.tsx
@@ -5,36 +5,41 @@ import WatchDescription from "../components/WatchDescription";
 const WatchMovie = () => {
   const { movie_id } = useParams<{ movie_id: string }>();
   const { data: movie } = useWatchDetails("movie", movie_id ?? "");
-console.log(movie);
+  console.log(movie);
 
   return (
     <div className="min-h-screen page pt-[60px]">
       <div className="mt-24 flex flex-col lg:flex-row lg:gap-[24px] p-[16px]lg:p-[24px] lg:max-w-[2200px] lg:mx-auto">
         <div className="primary flex-1 w-full lg:max-w-[calc(100%-424px)]">
-          <header>
-            {/* header here */}
-          </header>
-            <main>
-              <div className="relative pt-[56.25%] w-full overflow-hidden mb-[24px] rounded-lg">
-                {/* <iframe
+          <header>{/* header here */}</header>
+          <main>
+            <div className="relative pt-[56.25%] w-full overflow-hidden mb-[24px] rounded-lg">
+              {/* <iframe
                   className="absolute top-0 left-0 w-full h-full "
                   width="100%"
                   height="100%"
                   src={`https://vidsrc.xyz/embed/movie/${movie_id}`}
                   allowFullScreen
                 ></iframe> */}
-              </div>
+            </div>
 
-              <div className="rounded-lg flex align-center justify-between gap-[16px] -my-[12px] p-[16px] bg-[#1f1f1f]">
-                {/* player controls */}
-                Player controls here
-              </div>
-              <div className="rounded-lg bg-[#1f1f1f]  border-[#2f2f2f] p-[24px] mb-[24px]">
-                {/* description */}
-                <WatchDescription title={movie.title} rt={movie.runtime} date={movie.release_date} overview={movie.overview}/>
-              </div>
-            </main>
-          
+            <div className="rounded-lg flex align-center justify-between gap-[16px] -my-[12px] p-[16px] bg-[#1f1f1f]">
+              {/* player controls */}
+
+              <hr className="h-0.5 w-full bg-gray-800/30 text-white" />
+            </div>
+            <div className="rounded-lg bg-[#1f1f1f]  border-[#2f2f2f] p-[24px] mb-[24px]">
+              {/* description */}
+              {movie && (
+                <WatchDescription
+                  title={movie.title}
+                  rt={movie.runtime}
+                  date={movie.release_date}
+                  overview={movie.overview}
+                />
+              )}
+            </div>
+          </main>
         </div>
         <div className="secondary ">
           {/* right side with server choices and episodes for tv*/}
