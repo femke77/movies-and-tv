aa66c40cf5614c49d76eadc1552224665cdd99b6
femke77 <megan.meyers.388@gmail.com>
2025-03-04 18:45:44 -0800

    watch next stops at last episode of last season

--- src/components/WatchNextBtn.tsx
+++ src/components/WatchNextBtn.tsx
@@ -4,36 +4,42 @@ const WatchNextBtn = ({
   selectedSeason,
   setSelectedSeason,
   numSeasons,
-  currentSeasonLength
+  currentSeasonLength,
 }: {
   selectedEpisode: number;
   setSelectedEpisode: (episode: any) => void;
   selectedSeason: number;
   setSelectedSeason: (season: any) => void;
   numSeasons: number;
-  currentSeasonLength: number
+  currentSeasonLength: number;
 }) => {
-
   console.log(selectedEpisode, currentSeasonLength, numSeasons);
-  
-//  if selected episode is the last episode of the season AND there is another season, swtich to the next season, else disable
+
+  //  if selected episode is the last episode of the season AND there is another season, swtich to the next season, else disable
 
   // TODO make it go to next season if last episode of current season
- 
-   const handleNextEpisodeRequest = () => {
-    if (selectedEpisode === currentSeasonLength && selectedSeason < numSeasons) {
+
+  const handleNextEpisodeRequest = () => {
+    // switching to next season if available
+    if (
+      selectedEpisode === currentSeasonLength &&
+      selectedSeason < numSeasons
+    ) {
       setSelectedSeason(selectedSeason + 1);
       setSelectedEpisode(1);
-    } else {
-      setSelectedEpisode(selectedEpisode
-        + 1); 
+    } 
+    // stopping if last episode of last season
+    else if (
+      !(
+        selectedEpisode === currentSeasonLength && selectedSeason === numSeasons
+      )
+    ) {
+      setSelectedEpisode(selectedEpisode + 1);
+    }
+  };
 
-   }
-  }
- 
   return (
     <button
-   
       className={`next-button flex hover:cursor-pointer mx-2 bg-gray-700/50 p-2 px-4 pl-5 rounded-lg hover:bg-gray-700/70 hover:translate-[0.5px] active:translate-[0.5px] `}
       onClick={handleNextEpisodeRequest}
     >
