7cd6e5cacebd725ac110f66a15e8ce0c19f487d5
femke77 <megan.meyers.388@gmail.com>
2025-03-09 04:15:05 -0700

    fix for app Suspense

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,31 @@
-import { StrictMode, Suspense, lazy } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import ScrollToTop from './components/ScrollToTop.tsx';
-import ItemDetailSkeleton from './components/LoadingSkels/ItemCardSkeleton.tsx';
-import ErrorPage from './pages/404.tsx';
-import NotFound from './pages/404.tsx';
+import { StrictMode, Suspense, lazy } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ScrollToTop from "./components/ScrollToTop.tsx";
+import ItemDetailSkeleton from "./components/LoadingSkels/ItemCardSkeleton.tsx";
+import ErrorPage from "./pages/404.tsx";
+import NotFound from "./pages/404.tsx";
+import Loading from "./pages/Suspense.tsx";
 
-const DMCA = lazy(() => import('./pages/DMCA.tsx'));
-const ItemDetail = lazy(() => import('./pages/ItemDetail.tsx'));
-const Results = lazy(() => import('./pages/SearchPage.tsx'));
-const MovieTopRated = lazy(
-  () => import('./pages/moviePages/MovieTopRated.tsx'),
+const DMCA = lazy(() => import("./pages/DMCA.tsx"));
+const ItemDetail = lazy(() => import("./pages/ItemDetail.tsx"));
+const Results = lazy(() => import("./pages/SearchPage.tsx"));
+const MovieTopRated = lazy(() =>
+  import("./pages/moviePages/MovieTopRated.tsx")
 );
-const MoviePopular = lazy(() => import('./pages/moviePages/MoviePopular.tsx'));
-const MovieTrending = lazy(
-  () => import('./pages/moviePages/MovieTrending.tsx'),
+const MoviePopular = lazy(() => import("./pages/moviePages/MoviePopular.tsx"));
+const MovieTrending = lazy(() =>
+  import("./pages/moviePages/MovieTrending.tsx")
 );
-const TvTrending = lazy(() => import('./pages/tvPages/TvTrending.tsx'));
-const TvTopRated = lazy(() => import('./pages/tvPages/TvTopRated.tsx'));
-const TvPopular = lazy(() => import('./pages/tvPages/TvPopular.tsx'));
-const WatchMovie = lazy(() => import('./pages/watchPages/WatchMovie.tsx'));
-const WatchTV = lazy(() => import('./pages/watchPages/WatchTV.tsx'));
+const TvTrending = lazy(() => import("./pages/tvPages/TvTrending.tsx"));
+const TvTopRated = lazy(() => import("./pages/tvPages/TvTopRated.tsx"));
+const TvPopular = lazy(() => import("./pages/tvPages/TvPopular.tsx"));
+const WatchMovie = lazy(() => import("./pages/watchPages/WatchMovie.tsx"));
+const WatchTV = lazy(() => import("./pages/watchPages/WatchTV.tsx"));
 
 // TODO need to add something better than loading to suspense fallback or make new skels for the other pages
 
@@ -40,7 +41,7 @@ const queryClient = new QueryClient({
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     errorElement: <ErrorPage />,
 
     element: <App />,
@@ -49,17 +50,16 @@ const router = createBrowserRouter([
         index: true,
         element: <Home />,
       },
-
       {
-        path: 'search/:query?',
+        path: "search/:query?",
         element: <Results />,
       },
       {
-        path: 'dmca',
+        path: "dmca",
         element: <DMCA />,
       },
       {
-        path: ':item_type/:id',
+        path: ":item_type/:id",
         element: (
           <ScrollToTop>
             <Suspense fallback={<ItemDetailSkeleton />}>
@@ -69,63 +69,64 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: 'explore',
+        path: "explore",
         children: [
           {
-            path: 'movies',
+            path: "movies",
             element: <MovieTrending />,
           },
           {
-            path: 'toprated',
+            path: "toprated",
             element: <MovieTopRated />,
           },
           {
-            path: 'popular',
+            path: "popular",
             element: <MoviePopular />,
           },
           {
-            path: 'tv',
+            path: "tv",
             element: <TvTrending />,
           },
           {
-            path: 'top-series',
+            path: "top-series",
             element: <TvTopRated />,
           },
           {
-            path: 'popular-tv',
+            path: "popular-tv",
             element: <TvPopular />,
           },
         ],
       },
       {
-        path: 'watch',
+        path: "watch",
         children: [
           {
-            path: 'movie/:movie_id',
+            path: "movie/:movie_id",
             element: <WatchMovie />,
           },
           {
-            path: 'tv/:series_id',
+            path: "tv/:series_id",
             element: <WatchTV />,
           },
           {
-            path: 'tv/:series_id/:season_number/:episode_number',
+            path: "tv/:series_id/:season_number/:episode_number",
             element: <WatchTV />,
           },
         ],
       },
-      { path: '*', element: <NotFound /> },
+      { path: "*", element: <NotFound /> },
     ],
   },
 ]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      {/* TODO update this: */}
-      <Suspense fallback={'Loading....'}>
-      <RouterProvider router={router} />
+      <Suspense
+        fallback={<span className="loader min-h-screen flex mx-auto"></span>}
+      >
+        <RouterProvider router={router} />
       </Suspense>
     </QueryClientProvider>
-  </StrictMode>,
+  </StrictMode>
 );
