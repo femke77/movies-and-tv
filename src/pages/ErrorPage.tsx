62e802e0a60e3daf3e4d0bd9168bce2854fd2bf5
femke77 <megan.meyers.388@gmail.com>
2025-03-09 01:33:17 -0800

    error element and not found now separate pages and routes

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom';
 
-const NotFound = () => {
+const ErrorPage = () => {
   return (
     <section className='text-white/70 flex flex-col items-center justify-center h-screen text-4xl space-y-6'>
-      <h1>404: Page Not Found</h1>
+      <h1>An Unexpected Error Occured. We're Sorry!</h1>
       <h1> ¯\_(ツ)_/¯</h1>
       <Link to='/' className='text-blue-500 underline'>
         Go back home
@@ -12,4 +12,4 @@ const NotFound = () => {
   );
 };
 
-export default NotFound;
+export default ErrorPage;
