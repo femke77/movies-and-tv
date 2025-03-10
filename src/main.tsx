30e1f55a2a8c4199403734fd55b193184bdcd1de
femke77 <megan.meyers.388@gmail.com>
2025-03-10 02:18:06 -0700

    changed error boundary code for redeploys with lazy loaded pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { ErrorBoundary } from 'react-error-boundary';
 import { StrictMode, Suspense, lazy, Component } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
@@ -28,51 +29,32 @@ const WatchMovie = lazy(() => import('./pages/watchPages/WatchMovie.tsx'));
 const WatchTV = lazy(() => import('./pages/watchPages/WatchTV.tsx'));
 const DMCA = lazy(() => import('./pages/DMCA.tsx'));
 
-interface ChunkLoadErrorBoundaryProps {
-  children: React.ReactNode;
+interface IErrorProps {
+ name: string;
+ message: string;
 }
 
-class ChunkLoadErrorBoundary extends Component<ChunkLoadErrorBoundaryProps> {
-  state = { hasError: false };
+// Error fallback component
+const ErrorFallback = () => {
+  //  won't actually see this 
+  return <div>Reloading application...</div>;
+};
 
-  static getDerivedStateFromError(error: { message: string }) {
-    // for lazy loading 404 errors after a redeploy
-    if (error.message && error.message.includes('Loading chunk')) {
-      return { hasError: true };
-    }
-    return { hasError: false };
-  }
-
-  componentDidCatch(error: { message: string }) {
-    console.error('Chunk loading error:', error);
-  }
-
-  refreshPage = () => {
+// Error handler function for redeploys with lazy routes
+const handleError = (error: IErrorProps) => {
+  //  chunk load error?
+  if (
+    error.name === 'ChunkLoadError' || 
+    (error.message && (
+      error.message.includes('Loading chunk') || 
+      error.message.includes('Failed to fetch dynamically imported module')
+    ))
+  ) {
+    console.error("Chunk error caught:", error);
+    // Immediately reload the page
     window.location.reload();
-  };
-
-  render() {
-    if (this.state.hasError) {
-      return (
-        <div className='mt-40 text-center'>
-          <h2>Something went wrong</h2>
-          <p>
-            The app was likely updated. Please refresh to get the latest
-            version.
-          </p>
-          <button
-            className='bg-gray-800/50 text-white'
-            onClick={this.refreshPage}
-          >
-            Refresh Now
-          </button>
-        </div>
-      );
-    }
-
-    return this.props.children;
   }
-}
+};
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -171,13 +153,13 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ChunkLoadErrorBoundary>
+    <ErrorBoundary FallbackComponent={ErrorFallback} onError={handleError}>
         <Suspense
           fallback={<span className='loader min-h-screen flex mx-auto'></span>}
         >
           <RouterProvider router={router} />
         </Suspense>
-      </ChunkLoadErrorBoundary>
+      </ErrorBoundary>
     </QueryClientProvider>
   </StrictMode>,
 );
