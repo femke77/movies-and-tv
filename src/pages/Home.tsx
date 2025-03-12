df3b23d98aa9bdb0a32fa2fcd6e44284f2dd6eaf
femke77 <megan.meyers.388@gmail.com>
2025-03-12 00:44:50 -0700

    renamed directory home->main to fix deploy issue

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import { lazy } from 'react';
 
-import SwiperElement from '../components/home/Swiper';
+import SwiperElement from '../components/main/Swiper';
 
-const PopularMovies = lazy(() => import('../components/home/PopularMovies'));
-const TrendingMovies = lazy(() => import('../components/home/TrendingMovies'));
-const TopRatedMovies = lazy(() => import('../components/home/TopRatedMovies'));
-const PopularTv = lazy(() => import('../components/home/PopularTv'));
-const TrendingTV = lazy(() => import('../components/home/TrendingTv'));
-const TopRatedTv = lazy(() => import('../components/home/TopRatedTv'));
+const PopularMovies = lazy(() => import('../components/main/PopularMovies'));
+const TrendingMovies = lazy(() => import('../components/main/TrendingMovies'));
+const TopRatedMovies = lazy(() => import('../components/main/TopRatedMovies'));
+const PopularTv = lazy(() => import('../components/main/PopularTv'));
+const TrendingTV = lazy(() => import('../components/main/TrendingTv'));
+const TopRatedTv = lazy(() => import('../components/main/TopRatedTv'));
 
 const Home = () => {
   return (
