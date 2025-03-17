759e41af55303e5978568fd994ec9544e0073a00
femke77 <megan.meyers.388@gmail.com>
2025-03-16 21:25:13 -0700

    updated tooltip style

--- src/components/ToolTip.tsx
+++ src/components/ToolTip.tsx
@@ -5,9 +5,10 @@ const Tooltip = ({ children, text }: {
     return (
         <div role="tooltip" id="tooltip-id" className="relative group z-100">
         {children}
+        {/* left-1/2 transform -translate-x-1/2 top-full mt-2 */}
         <div
-          className="text-md max-w-xs absolute shadow-lg hidden group-hover:block bg-black text-white font-semibold px-3 py-[6px] text-[13px] left-full ml-4 top-1/2 -translate-y-1/2 w-max rounded 
-          before:w-4 before:h-4 before:rotate-45 before:bg-black before:absolute before:z-[-1] before:top-1/2 before:-left-2 before:-translate-y-1/2 before:ml-1">
+          className="text-md max-w-xs font-semibold shadow-lg hidden group-hover:block bg-white text-black  px-3 py-[6px] text-[13px] absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 w-max rounded 
+          before:w-4 before:h-4 before:rotate-45 before:bg-white before:absolute before:z-[-1] before:top-5 before:left-1/2 before:-translate-x-1/2 ">
           {text}
         </div>
       </div>
