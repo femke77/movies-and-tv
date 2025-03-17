49a4eb30267ee2df54144b8bb521331363fa4231
femke77 <megan.meyers.388@gmail.com>
2025-03-16 22:32:37 -0700

    serverlist & serverbtn refactored to accept state and onclick updates selected server correctly on tv and movie - no change of url yet

--- src/pages/watchPages/WatchMovie.tsx
+++ src/pages/watchPages/WatchMovie.tsx
@@ -1,79 +1,77 @@
-import { useParams } from 'react-router-dom';
-import { useWatchDetails } from '../../hooks/useItemOrWatchDetail';
-import WatchDescription from '../../components/WatchDescription';
-import BackButton from '../../components/buttons/BackBtn';
-import FullscreenBtn from '../../components/buttons/FullScreenBtn';
-import ServerList from '../../components/buttons/ServerButton';
-import { isIphoneSafari } from '../../utils/helpers';
+import { useParams } from "react-router-dom";
+import { useWatchDetails } from "../../hooks/useItemOrWatchDetail";
+import WatchDescription from "../../components/WatchDescription";
+import BackButton from "../../components/buttons/BackBtn";
+import FullscreenBtn from "../../components/buttons/FullScreenBtn";
+import ServerList from "../../components/ServerList";
+import { isIphoneSafari } from "../../utils/helpers";
+import serverData from "../../utils/data/servers.json";
+import { useState } from "react";
 
 const WatchMovie = () => {
   const { movie_id } = useParams<{ movie_id: string }>();
-  const { data: movie = {} } = useWatchDetails('movie', movie_id ?? '');
+  const { data: movie = {} } = useWatchDetails("movie", movie_id ?? "");
+  const [selectedServer, setSelectedServer] = useState(serverData.servers[0].name || "");
+  const { servers } = serverData;
 
-  // const getPlayBackUrl = () => {
-  //   if (!isIPad()) {
-  //     return `/api/video/movie/${movie_id}`;
-  //   }
-  //   return `https://vidsrc.xyz/embed/movie/${movie_id}`;
-  // }
-
+  
   return (
-    <div className='min-h-screen  pt-[60px]'>
-      <div className='flex flex-col lg:flex-row lg:gap-[24px] p-[16px] lg:p-[24px] lg:max-w-[2200px] lg:mx-auto'>
-        <div className='primary flex-1 w-full lg:max-w-[calc(100%-424px)]'>
-          <div className='flex items-center justify-between text-xl mb-[16px] rounded-lg bg-[#1f1f1f] py-[12px] px-[16px]'>
+    <div className="min-h-screen  pt-[60px]">
+      <div className="flex flex-col lg:flex-row lg:gap-[24px] p-[16px] lg:p-[24px] lg:max-w-[2200px] lg:mx-auto">
+        <div className="primary flex-1 w-full lg:max-w-[calc(100%-424px)]">
+          <div className="flex items-center justify-between text-xl mb-[16px] rounded-lg bg-[#1f1f1f] py-[12px] px-[16px]">
             <div>
               <BackButton />
             </div>
             {movie && (
               <p
-                className='font-bold truncate text-ellipsis mx-6'
-                title={movie.title}
+              className="font-bold truncate text-ellipsis mx-6"
+              title={movie.title}
               >
-                {movie.title || ''}
+                {movie.title || ""}
               </p>
             )}
 
-            <div className={`${isIphoneSafari() ? 'invisible' : ''}`}>
-              <FullscreenBtn elementId='iframe' />
+            <div className={`${isIphoneSafari() ? "invisible" : ""}`}>
+              <FullscreenBtn elementId="iframe" />
             </div>
           </div>
           <main>
-            <div className='relative pt-[56.25%] w-full overflow-hidden mb-[24px] rounded-lg bg-[#1f1f1f]'>
+            <div className="relative pt-[56.25%] w-full overflow-hidden mb-[24px] rounded-lg bg-[#1f1f1f]">
               <iframe
-                id='iframe'
-                className='absolute top-0 left-0 w-full h-full '
-                width='100%'
-                height='100%'
+                id="iframe"
+                className="absolute top-0 left-0 w-full h-full "
+                width="100%"
+                height="100%"
                 // {...(!isIPad() && {
-                //   sandbox: 'allow-scripts allow-same-origin allow-presentation',
-                // })}
-                // src={getPlayBackUrl()}
-                // sandbox="allow-scripts allow-same-origin allow-presentation"
-                // src={`/api/video/movie/${movie_id}`}
-
-                src={`https://vidsrc.xyz/embed/movie/${movie_id}`}
-                allowFullScreen
-              ></iframe>
+                  //   sandbox: 'allow-scripts allow-same-origin allow-presentation',
+                  // })}
+                  // src={getPlayBackUrl()}
+                  // sandbox="allow-scripts allow-same-origin allow-presentation"
+                  // src={`/api/video/movie/${movie_id}`}
+                  
+                  src={`https://vidsrc.xyz/embed/movie/${movie_id}`}
+                  allowFullScreen
+                  ></iframe>
             </div>
 
-            <div className='rounded-lg bg-[#1f1f1f]  border-[#2f2f2f] p-[24px] mb-[24px]'>
+            <div className="rounded-lg bg-[#1f1f1f]  border-[#2f2f2f] p-[24px] mb-[24px]">
               {/* description */}
               {movie && (
                 <WatchDescription
-                  title={movie?.title}
-                  rt={movie?.runtime}
-                  date={movie?.release_date}
-                  overview={movie?.overview || 'No summary available'}
+                title={movie?.title}
+                rt={movie?.runtime}
+                date={movie?.release_date}
+                overview={movie?.overview || "No summary available"}
                 />
               )}
             </div>
           </main>
         </div>
-        <div className='secondary lg:w-[400px] lg:flex-shrink-0 '>
+        <div className="secondary lg:w-[400px] lg:flex-shrink-0 ">
           {/* right side with server choices and episodes for tv*/}
-          <div className='sidebar bg-[#1f1f1f] max-h-[800px] flex flex-col  rounded-lg'>
-            <ServerList />
+          <div className="sidebar bg-[#1f1f1f] max-h-[800px] flex flex-col  rounded-lg">
+            <ServerList serverData={servers} selectedServer={selectedServer} setSelectedServer={setSelectedServer}/>
           </div>
         </div>
       </div>
@@ -82,3 +80,24 @@ const WatchMovie = () => {
 };
 
 export default WatchMovie;
+
+
+
+
+// const getPlayBackUrl = () => {
+  //   if (!isIPad()) {
+    //     return `/api/video/movie/${movie_id}`;
+    //   }
+    //   return `https://vidsrc.xyz/embed/movie/${movie_id}`;
+    // }
+    
+    //   useEffect(() => {
+    //   window.addEventListener("message", function (event) {
+    //     console.log("event: ", event);
+    //     // console.log("Message received from the player: ", JSON.parse(event.data)); // Message received from player
+    //     // if (typeof event.data === "string") {
+    //     //   var messageArea = document.querySelector("#messageArea");
+    //     //   if (messageArea) messageArea.innerText = event.data;
+    //     // }
+    //   });
+    // }, []);
