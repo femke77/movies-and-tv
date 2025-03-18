651c491fa6ea06f280fd7279bdd47432294113cc
femke77 <megan.meyers.388@gmail.com>
2025-03-17 20:54:51 -0700

    refactored back btn to be used to skip going back through server changes and episode changes and go back to details page

--- src/components/buttons/BackBtn.tsx
+++ src/components/buttons/BackBtn.tsx
@@ -1,25 +1,14 @@
+import { ArrowLeft } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
-const BackButton = () => {
+const BackButton = ({url}: {url: string}) => {
+  const navigate = useNavigate();
   return (
     <button
       className='back-button flex hover:cursor-pointer'
-      onClick={() => history.back()}
+      onClick={() => navigate(url)}
     >
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        width='20'
-        height='20'
-        viewBox='0 0 24 24'
-        fill='none'
-        stroke='currentColor'
-        strokeWidth='2'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        className='lucide lucide-arrow-left'
-      >
-        <path d='m12 19-7-7 7-7'></path>
-        <path d='M19 12H5'></path>
-      </svg>
+      <ArrowLeft size={20} color='#ffffff' />
       <p className='text-sm ml-1'> Back</p>
     </button>
   );
