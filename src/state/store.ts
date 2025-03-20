a84e23da24d1bbc03c0feea1a2ef73926e54ae2c
femke77 <megan.meyers.388@gmail.com>
2025-03-19 21:31:31 -0700

    minor fix to store

--- src/state/store.ts
+++ src/state/store.ts
@@ -13,45 +13,45 @@ interface BookmarkStore {
 }
 
 export const useBookmarkStore = create<BookmarkStore>()(
-  persist(
-    (set, get) => ({
-      bookmarks: [],
-      modalData: null, // Store the item being bookmarked
-      showModal: false,
-
-      openModal: (id: string, type: string) => {
-        set({
-          modalData: {
-            id,
-            type,
-            isBookmarked: get().isBookmarked(id, type),
-          },
-          showModal: true,
-        });
-      },
-      closeModal: () => set({ showModal: false, modalData: null }),
-
-      addBookmark: (id, type) =>
-        set((state) => ({
-          bookmarks: [...state.bookmarks, { id, type }],
-        })),
-
-      removeBookmark: (id, type) =>
-        set((state) => ({
-          bookmarks: state.bookmarks.filter(
-            (b) => !(b.id === id && b.type === type),
-          ),
-        })),
-
-      isBookmarked: (id, type) =>
-        get().bookmarks.some((b) => b.id === id && b.type === type),
-    }),
-    {
-      name: 'bookmark-storage',
-      storage: createJSONStorage(() => localStorage),
-      partialize: (state) => ({
-        bookmarks: state.bookmarks,
+    persist(
+      (set, get) => ({
+        bookmarks: [],
+        modalData: null, // Store the item being bookmarked
+        showModal: false, 
+  
+        openModal: (id: string, type: string) => {
+          set({
+            modalData: {
+              id,
+              type,
+              isBookmarked: get().isBookmarked(id, type),
+            },
+            showModal: true,
+          });
+        },
+        closeModal: () => set({ showModal: false, modalData: null }),
+  
+        addBookmark: (id, type) =>
+          set((state) => ({
+            bookmarks: [...state.bookmarks, { id, type }],
+          })),
+  
+        removeBookmark: (id, type) =>
+          set((state) => ({
+            bookmarks: state.bookmarks.filter(
+              (b) => !(b.id === id && b.type === type)
+            ),
+          })),
+  
+        isBookmarked: (id, type) =>
+          get().bookmarks.some((b) => b.id === id && b.type === type),
       }),
-    },
-  ),
-);
+      {
+        name: "bookmark-storage",
+        storage: createJSONStorage(() => localStorage),
+        partialize: (state) => ({
+          bookmarks: state.bookmarks,
+        }),
+      }
+    )
+  );
