00279a01f87311db539e12a0b3306faf931abc1d
femke77 <megan.meyers.388@gmail.com>
2025-03-20 15:26:17 -0700

    updated mobile nav with new section - my bingebox

--- src/pages/SavedItems.tsx
+++ src/pages/SavedItems.tsx
@@ -41,6 +41,7 @@ const SavedItems = () => {
 
   const isLoadingTv = tvQueries.some((query) => query.isLoading);
 
+  // prep loading skeletons
   const movieSkeletons = Array(movieBookmarks.length || 5).fill(null);
   const tvSkeletons = Array(movieBookmarks.length || 5).fill(null);
 
@@ -66,6 +67,7 @@ const SavedItems = () => {
         </div>
       )}
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4'>
+           
         {movieDetails.map((item) => (
           <ItemCard
             key={item.id}
