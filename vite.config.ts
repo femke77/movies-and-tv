dca8f7062bc31f29920dedeafe35af4aef58791c
femke77 <megan.meyers.388@gmail.com>
2025-03-28 00:21:04 -0700

    added pwa icons, manifest, precaching, autoUpdate of new content, terser

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,45 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from "vite-plugin-pwa";
+import analyze from "rollup-plugin-analyzer";
+import iconsData from './public/icons.json'
+
+const {icons} = iconsData;
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  build: {
+      minify: "terser",
+      rollupOptions: {
+        plugins: [process.env.NODE_ENV === 'production' ? null : analyze()].filter(Boolean),
+      },
+    },
+  plugins: [react(), tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+        type: "module",
+        navigateFallback: "index.html",
+      },
+      manifest: {
+        name: "BingeBox247 Unlimited Streaming",
+        short_name: "BingeBox",
+        description: "Keep track of and stream your favorite movies and TV shows with BingeBox247.",
+        theme_color: "#111",
+        icons: icons
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,jsx}"],
+        skipWaiting: true,
+        clientsClaim: true,
+        cleanupOutdatedCaches: true,
+        navigateFallback: "/index.html",
+      }
+    }),
+  ],
   server: {
     port: 3003,
     host: true,
