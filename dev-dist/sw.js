0cb5eedbd5e33d205b6bb97e2d3f1c347fcdcc6d
femke77 <megan.meyers.388@gmail.com>
2025-04-02 22:12:29 -0700

    pwa autoupdate again

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -69,11 +69,7 @@ if (!self.define) {
 }
 define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
 
-  self.addEventListener('message', event => {
-    if (event.data && event.data.type === 'SKIP_WAITING') {
-      self.skipWaiting();
-    }
-  });
+  self.skipWaiting();
   workbox.clientsClaim();
 
   /**
@@ -86,7 +82,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.p4iou7lsuv8"
+    "revision": "0.rqq8el7sjtg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
