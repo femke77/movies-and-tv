5f96daee7b1274daf448e600722d92b0b3feda64
femke77 <megan.meyers.388@gmail.com>
2025-04-06 20:37:13 -0700

    css update

--- src/main.tsx
+++ src/main.tsx
@@ -70,7 +70,7 @@ const router = createBrowserRouter([
         element: (
           <KeepAlive saveScrollPosition='screen' name='home'>
             <Home />
-          </KeepAlive>
+           </KeepAlive>
         ),
       },
       {
