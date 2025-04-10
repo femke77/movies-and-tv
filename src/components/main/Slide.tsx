de1b676615e1ed96fb6645652cc55c674812d67d
femke77 <megan.meyers.388@gmail.com>
2025-04-10 03:39:27 -0700

    finally got the janky brave issue fixed for real! no for real this time

--- src/components/main/Slide.tsx
+++ src/components/main/Slide.tsx
@@ -116,7 +116,7 @@ const Slide = ({
 
   return (
     <div
-      className={` swiper-slide bg-black h-full flex items-center slide-container overflow-hidden ${
+      className={` swiper-slide bg-black max-h-screen flex items-center slide-container overflow-hidden ${
         isVisible ? 'visible' : 'invisible'
       }`}
     >
@@ -128,6 +128,7 @@ const Slide = ({
         style={{
           backgroundImage: `url('https://image.tmdb.org/t/p/w1280${slide.backdrop_path}')`,
           backgroundSize: '100% auto',
+          backgroundRepeat: 'no-repeat',
         }}
       >
         {/* gradient overlays */}
