844058f74246712eedd9a7c967ffd49f28629326
femke77 <megan.meyers.388@gmail.com>
2025-04-11 00:30:06 -0700

    setting up netlify function for video quality feature

---
+++ src/netlify/functions/api-proxy.ts
@@ -0,0 +1,28 @@
+import { Handler } from '@netlify/functions'
+
+
+const handler: Handler = async (event) => {
+  const path = event.path.replace('/.netlify/functions/api-proxy', '')
+  const url = `https://api.offlinetv.net${path}${event.rawQuery ? '?' + event.rawQuery : ''}`
+
+  const res = await fetch(url, {
+    method: event.httpMethod,
+    headers: {
+      ...event.headers,
+      host: 'api.offlinetv.net',
+    },
+    body: event.body,
+  })
+
+  const body = await res.text()
+
+  return {
+    statusCode: res.status,
+    headers: {
+      'content-type': res.headers.get('content-type') || 'application/json',
+    },
+    body,
+  }
+}
+
+export { handler }
